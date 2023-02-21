97390a0378ff07be24d2aa4943526260a59cdab9
theocesar <theocesar@live.com>
2023-02-21 15:54:29 -0300

    Update Main.java

--- Parallell_programming/Main.java
+++ Parallell_programming/Main.java
@@ -6,15 +6,18 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
+        // creating the semaphores.
         Semaphore a = new Semaphore(1);
         Semaphore b = new Semaphore(0);
         Semaphore c = new Semaphore(1);
         Semaphore d = new Semaphore(0);
 
+        // creating the threads.
         Criador cr = new Criador(a, b);
         Tratador t = new Tratador(b, d, c, a, cr);
         Analisador ans = new Analisador(d, c, t);
 
+        // starting the threads.
         cr.start();
         t.start();
         ans.start();
